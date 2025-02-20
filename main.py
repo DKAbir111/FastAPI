5b32197193fe4f8ff10e15b85e7e232bbc526e0d
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2025-02-20 23:14:32 +0600

    user db created and send res by get route

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 from fastapi import FastAPI
+
 app=FastAPI()
 
+
+users_db=[
+        {
+            "name": "John Doe",
+            "email": "johndoe@example.com",
+            "age": 28
+        },
+        {
+            "name": "Jane Smith",
+            "email": "janesmith@example.com",
+            "age": 32
+        },
+        {
+            "name": "Alice Johnson",
+            "email": "alicejohnson@example.com",
+            "age": 25
+        },
+        {
+            "name": "Michael Brown",
+            "email": "michaelbrown@example.com",
+            "age": 40
+        }
+    ]
 @app.get('/')
 async def root():
-    return {'message': 'Hello, World!'}
+    return {"Server is running on http://127.0.0.1:8000"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id:int):
-    return {'item_id': item_id}
+
+@app.get('/users')
+async def get_users():
+    return users_db
